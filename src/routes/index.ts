b0e3f57d817a625d4f68bf182c652384dbc17b3c
Mayur B Patel <mayur.b.patel@1rivet.com>
2024-07-24 15:22:31 +0530

    Fix : removed routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,16 +8,5 @@ router.post('/register', (req: Request, res: Response): void => {
   }
   res.status(201).json({ message: 'User created' });
 });
-router.post('/login', (req: Request, res: Response): void => {
-  if (!req.body.firstName) {
-    res.status(400).json({ message: 'First name is required' });
-  }
-  res.status(201).json({ message: 'User created' });
-});
-router.post('/login', (req: Request, res: Response): void => {
-  if (!req.body.firstName) {
-    res.status(400).json({ message: 'First name is required' });
-  }
-  res.status(201).json({ message: 'User created' });
-});
+
 export default router;
